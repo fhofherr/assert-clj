fbdd5b93a46fd92499a0227bdb37931cf9650951
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-06-07 17:09:46 +0200

    Update assertj-core.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache v2 License"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.assertj/assertj-core "1.6.0"]]
+                 [org.assertj/assertj-core "1.6.1"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/test.check "0.5.8"]
                                   [org.clojure/tools.namespace "0.2.4"]]}})
