8f02172f8257afe119fc8c6faf559109a6f1303f
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2014-06-07 14:49:43 +0200

    Restructure tests.

--- test/assert_clj/api_test.clj
+++ test/assert_clj/api_test.clj
@@ -9,32 +9,46 @@
 
 (def some-actual (gen/return (gen/sample gen/any 1)))
 
-(defn is-assertj-assert? [a] (instance? Assert a))
+(defn is-assertj-assert? [a] (is (instance? Assert a)))
+(defn has-description-text
+  [txt a]
+  (is (= txt (.descriptionText a))))
 
-(defspec assert-that-creates-assertj-assertion 10
-  (prop/for-all [v gen/any]
-                (is-assertj-assert? (api/assert-that v))))
+(deftest assert-that-creates-assertj-assertion
+  (testing "without calling any assert* methods"
+    (tc/quick-check
+      10
+      (prop/for-all [v gen/any]
+                    (is-assertj-assert? (api/assert-that v)))))
 
-(defspec assert-that-allows-to-call-the-assertions-methods 10
-  (prop/for-all [v gen/any]
-                (api/assert-that v
-                                 (isEqualTo v)
-                                 (isSameAs v))))
+  (testing "with calling some assert* methods"
+    (tc/quick-check
+      10
+      (prop/for-all [v gen/any]
+                    (api/assert-that v
+                                     (isEqualTo v)
+                                     (isSameAs v))))))
 
-(defspec assert-that-allows-to-add-a-docstring-right-after-the-actual 10
-  (prop/for-all [v gen/any]
-                 (= "Hello World"
-                    (.descriptionText (api/assert-that v "Hello World")))))
+(deftest assert-that-allows-to-add-a-docstring-right-after-the-actual
+  (testing "a simple docstring"
+    (tc/quick-check
+      10
+      (prop/for-all [v gen/any]
+                    (has-description-text "Hello World"
+                                          (api/assert-that v "Hello World")))))
 
-(defspec assert-that-allows-to-store-docstrings-in-vars 10
-  (prop/for-all [v gen/any
-                 s gen/string-ascii]
-                (= s (.descriptionText (api/assert-that v s)))))
+  (testing "storage of docstring in vars"
+    (tc/quick-check
+      10
+      (prop/for-all [v gen/any
+                     s gen/string-ascii]
+                    (has-description-text s (api/assert-that v s)))))
 
-(deftest assert-that-fails-if-docstring-var-is-no-string
-  (tc/quick-check
-    10
-    (prop/for-all [v gen/any
-                   s (gen/such-that (comp not string?) gen/any)]
-                  (is (thrown? IllegalArgumentException
-                               (api/assert-that v s))))))
+  (testing "failure if var does not contain string"
+    (tc/quick-check
+      10
+      (prop/for-all [v gen/any
+                     s (gen/such-that (comp not string?) gen/any)]
+                    (is
+                      (thrown? IllegalArgumentException
+                               (api/assert-that v s)))))))
